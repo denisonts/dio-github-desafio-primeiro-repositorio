915e80f7a4cbb6243f4a6a1191757973ac068bc6
Denison TS <106643117+denisonts@users.noreply.github.com>
2022-07-06 00:35:33 -0300

    Algoritimo, resolução do URI 1011 em C#, como parte do Desafio GIT e GITHUB

---
+++ Udemy 1011/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Udemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, maior, maiorAB;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+
+            maiorAB = ((A + B + (Math.Abs(A - B))) / 2);
+            maior = ((maiorAB + C + (Math.Abs(maiorAB - C))) / 2);
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
